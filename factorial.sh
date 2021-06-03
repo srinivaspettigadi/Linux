596afd4036b6bea857e13cda543f302bd55fc440
srinivaspettigadi <40118279+srinivaspettigadi@users.noreply.github.com>
2021-06-03 21:08:18 +0530

    Create factorial.sh

---
+++ factorial.sh
@@ -0,0 +1,11 @@
+cd /home/srinivas/linux
+chmod 777 *
+echo -n "enter the number: "
+read -r a
+fact=1
+while [ $a -ne 0 ] ; do
+	fact=$((fact*a))
+	echo "checking the fact value step by step" $fact
+	a=$((a-1))
+done
+echo $fact
