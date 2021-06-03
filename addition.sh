5620173e4802ab87980653d068c8fc4937417397
srinivaspettigadi <40118279+srinivaspettigadi@users.noreply.github.com>
2021-06-03 21:07:45 +0530

    Create addition.sh

---
+++ addition.sh
@@ -0,0 +1,25 @@
+Addition of two numbers only if you enter numerics
+Usage of unset here in below addition script is
+
+./unset.sh
+var="srinivas"
+echo $var
+unset var
+echo $var
+var="nani"
+echo $var
+unset var
+echo $var
+
+unset is used to unset the value of any variable locally
+
+unset a
+unset b
+until [[ $a == +([0-9]) ]] ; do
+	read -r -p "please enter first number:" a
+done
+until [[ $b == +([0-9]) ]] ; do
+	read -r -p "please enter second number:" b
+done
+echo "$a + $b = $((a+b))"
+
