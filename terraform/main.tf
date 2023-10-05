df9c8b83b4ed22e6a247ac97c23e0454c9c37783
srinivaspettigadi <40118279+srinivaspettigadi@users.noreply.github.com>
2023-10-05 23:31:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_vpc" "example_vpc" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
 }
 
@@ -30,8 +30,12 @@ resource "aws_vpn_connection" "example_vpn_connection" {
   type               = "ipsec.1"
 }
 
+resource "aws_route_table" "example_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+}
+
 resource "aws_route" "example_vpn_route" {
-  route_table_id         = aws_subnet.example_subnet.route_table_id
+  route_table_id         = aws_route_table.example_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_vpn_gateway.example_vpn_gateway.id
 }
