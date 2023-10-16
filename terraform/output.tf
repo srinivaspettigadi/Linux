5e00adf973d3f0f8efba3d20497a5678e4870744
srinivaspettigadi <40118279+srinivaspettigadi@users.noreply.github.com>
2023-10-16 22:48:14 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
 output "vpn_gateway_id" {
-  value = aws_vpn_gateway.example_vpn_gateway.id
+  value = aws_vpn_gateway.test_vpn_gateway.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.example_vpc.id
+  value = aws_vpc.test_vpc.id
 }
 
 # Add any additional outputs you want to display here.
