518272cc56ae5a951cf36061432c5ac794428719
srinivaspettigadi <40118279+srinivaspettigadi@users.noreply.github.com>
2023-10-19 23:15:10 +0530

    Create variables.tf

---
+++ terraform/VPG/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region where VGW will be created."
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC to associate with the VGW."
+}
+
+# Define other variables required for your VGW module
