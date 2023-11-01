5e2430fc3e46b1186a47dfd1782a08ec4042ed8f
srinivaspettigadi <40118279+srinivaspettigadi@users.noreply.github.com>
2023-11-01 22:49:01 +0530

    Create variables.tf

---
+++ terraform/first/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_name" {
+  description = "Name for the VPC"
+  type        = string
+  default     = "BMDEV-TF"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "172.30.0.0/16"
+}
