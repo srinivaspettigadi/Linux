779f111c4b8ec973e2fbcd3d484faba445991cfe
srinivaspettigadi <40118279+srinivaspettigadi@users.noreply.github.com>
2023-11-07 23:21:52 +0530

    Update main.tf

--- terraform/subnet/main.tf
+++ terraform/subnet/main.tf
@@ -5,13 +5,13 @@ variable "vpc_id" {
 resource "aws_subnet" "rap1_subnet_tf" {
   vpc_id                  = var.vpc_id
   cidr_block              = "172.30.200.0/24"
-  availability_zone       = "us-east-1a"  # Adjust the AZ as needed
+  availability_zone       = "us-east-2a"  # Adjust the AZ as needed
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "wap1_subnet_tf" {
   vpc_id                  = var.vpc_id
   cidr_block              = "172.30.50.0/24"
-  availability_zone       = "us-east-1b"  # Adjust the AZ as needed
+  availability_zone       = "us-east-2b"  # Adjust the AZ as needed
   map_public_ip_on_launch = true
 }
