6c3cf91d0253ecbca7af03a6a2845bbf57d6234d
srinivaspettigadi <40118279+srinivaspettigadi@users.noreply.github.com>
2023-11-07 23:21:13 +0530

    Create main.tf

---
+++ terraform/subnet/main.tf
@@ -0,0 +1,17 @@
+variable "vpc_id" {
+  description = "ID of the existing VPC"
+}
+
+resource "aws_subnet" "rap1_subnet_tf" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "172.30.200.0/24"
+  availability_zone       = "us-east-1a"  # Adjust the AZ as needed
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "wap1_subnet_tf" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "172.30.50.0/24"
+  availability_zone       = "us-east-1b"  # Adjust the AZ as needed
+  map_public_ip_on_launch = true
+}
